31d85b1b63428dca491c812969a7f1a7cfac87c8
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-07-12 23:15:16 -0300

    Probando rutas simples

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { get } from 'http';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+//CREANDO ENPOINS EN CONTROLADORES Y DEVOLVER UNA RESPUESTA
 
-  @Get()
+  @Get()   //ruta principal el @Get es un decorador
   getHello(): string {
-    return this.appService.getHello();
+    return "Hola Mundo"
+  }
+  @Get("nuevo")   //ruta normal
+  newEndpoint(){
+    return "soy un nuevo enpiont"
+  }
+  @Get("/ruta/")  //ruta funcionado con slash o sin el
+  hello(){
+    return "soy una ruta con slash"
   }
 }
